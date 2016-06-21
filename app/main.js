b325e8e59e7a5adaa3bc815f6d07f0e773aba540
Geoffrey <geoffrey.trambolho@gmail.com>
2016-06-21 17:58:50 +0200

    clean ignored files

--- app/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var application_1 = require("nativescript-angular/application");
-var app_component_1 = require("./app.component");
-var status_bar_util_1 = require("./utils/status-bar-util");
-status_bar_util_1.setStatusBarColors();
-application_1.nativeScriptBootstrap(app_component_1.AppComponent, null, { startPageActionBarHidden: false });
-//# sourceMappingURL=main.js.map
